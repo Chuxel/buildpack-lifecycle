06f607c1be0142b28f7923b7369411c8d0784a51
Chuck Lantz <chuck_lantz@hotmail.com>
2022-04-01 21:14:26 +0000

    Fix

--- testdata/image-test/test-image.sh
+++ testdata/image-test/test-image.sh
@@ -7,10 +7,14 @@ LIFECYCLE_IMAGE_TAG="${3:-latest}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}/../../"
 
+echo "(*) Building lifecycle binaries..."
+make clean
 make build-linux-amd64 
+echo "(*) Packaging lifecycle binaries..."
 make package-linux-amd64 
 cp -f out/lifecycle-v*+linux.x86-64.tgz "${SCRIPT_DIR}/lifecycle.tgz"
 
+echo "(*) Creating builder.."
 cd "${SCRIPT_DIR}"
 pack builder create "ghcr.io/chuxel/lifecycle/builder-devcontainer-empty" --pull-policy if-not-present -c builder-devcontainer.toml
 rm "${SCRIPT_DIR}/lifecycle.tgz"
@@ -25,4 +29,5 @@ pack build lifecycle-image-test-output \
     --lifecycle-image "${IMAGE_NAME}:${LIFECYCLE_IMAGE_TAG}" \
     -p "${BUILD_PATH}"
 
+echo "(*) Listing layers in output.."
 docker run -it --rm --entrypoint /cnb/lifecycle/launcher lifecycle-image-test-output ls /layers
